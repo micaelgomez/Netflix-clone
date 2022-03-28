60660b0ca9938fe3d5d1fd9761a9de7c06a599a1
micael <mickii.gomez.mg@gmail.com>
2022-03-27 22:53:29 -0300

    1 parte backend

---
+++ api/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connection Successfull!"))
+  .catch((err) => console.log(err));
+
+//Permite que expres soporte el json
+app.use(express.json());
+
+//ruta
+app.use("/api/auth", authRoute);
+
+app.listen(8800, () => {
+  console.log("Backend server is running !");
+});
